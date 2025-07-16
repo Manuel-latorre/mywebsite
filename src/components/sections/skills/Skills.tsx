7b351193abd5a0cea909156b5bb04c791f183503
Manuel-latorre <manuel.latorre11@gmail.com>
2025-07-16 18:17:39 -0300

    Update Skills component to enhance visual representation; replace heading with a class-based title, add new icon components for ReactQuery, Zustand, V0, Cursor, and Framer, and improve layout responsiveness with additional styling adjustments.

--- src/components/sections/skills/Skills.tsx
+++ src/components/sections/skills/Skills.tsx
@@ -1,14 +1,19 @@
+import Cursor from "@/components/icons/CursorIcon";
+import Framer from "@/components/icons/FramerMotion";
+import ReactQuery from "@/components/icons/ReactQueryIcon";
+import V0Icon from "@/components/icons/V0Icon";
+import ZustandIcon from "@/components/icons/ZustandIcon";
 import Badge from "@/components/ui/badge";
 
 const Skills = () => {
   return (
     <section
       id="skills"
-      className="min-h-dvh w-full max-w-7xl mx-auto flex items-start justify-center flex-col gap-10"
+      className="lg:min-h-dvh w-full max-w-7xl mx-auto flex lg:items-start justify-center flex-col gap-10"
     >
-      <h2 className="text-start text-6xl">Skills & Tools</h2>
+      <h2 className="title">Skills & Tools</h2>
 
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap gap-2 max-lg:justify-center max-lg:px-4">
         <img
           src="https://skillicons.dev/icons?i=html"
           className="hover:scale-105 transition-all duration-300"
@@ -53,9 +58,30 @@ const Skills = () => {
           src="https://skillicons.dev/icons?i=supabase"
           className="hover:scale-105 transition-all duration-300"
         />
+        <ReactQuery width={37} height={37} />
+        <ZustandIcon width={37} height={37} />
+        <img
+          src="https://skillicons.dev/icons?i=vitest"
+          className="hover:scale-105 transition-all duration-300"
+        />
+        <img
+          src="https://skillicons.dev/icons?i=git"
+          className="hover:scale-105 transition-all duration-300"
+        />
+        <img
+          src="https://skillicons.dev/icons?i=docker"
+          className="hover:scale-105 transition-all duration-300"
+        />
+        <img
+          src="https://skillicons.dev/icons?i=vercel"
+          className="hover:scale-105 transition-all duration-300"
+        />
+        <V0Icon width={37} height={37} />
+        <Cursor width={37} height={37} />
+        <Framer width={37} height={37} />
       </div>
 
-      <div className="flex flex-wrap items-center gap-2">
+      <div className="flex flex-wrap items-center gap-2 max-lg:justify-center max-lg:px-4">
         <Badge skill="proactive" />
         <Badge skill="autodidact" />
         <Badge skill="teamwork" />
