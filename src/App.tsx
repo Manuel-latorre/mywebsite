9e0f7d4524308ab300816b9c8bdc813748ef5b6e
Manuel-latorre <manuel.latorre11@gmail.com>
2025-07-17 00:26:27 -0300

    Refactor App component to improve code clarity by consolidating import statements and removing unnecessary whitespace, enhancing overall readability.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { ThemeProvider } from "./contexts/ThemeContext";
-import { LanguageProvider } from "./contexts/LanguageContext";
+import { ThemeProvider } from "./contexts/ThemeContext";import { LanguageProvider } from "./contexts/LanguageContext";
 import Navbar from "./components/navbar/Navbar";
 import Presentation from "./components/sections/presentation/Presentation";
 import Experience from "./components/sections/experience/Experience";
 import Skills from "./components/sections/skills/Skills";
 import ContactForm from "./components/sections/contact/ContactForm";
-import { Toaster } from "@/components/ui/sonner"
+import { Toaster } from "@/components/ui/sonner";
 import Mobilenav from "./components/navbar/Mobilenav";
 import { BackgroundLines } from "./components/ui/background-lines";
 
@@ -14,19 +13,13 @@ function App() {
     <ThemeProvider>
       <LanguageProvider>
         <Toaster />
-        
-
         <BackgroundLines>
-       
-            <Navbar />
-            <Mobilenav />
-
-            <Presentation />
-
-            <Experience/>
-
-            <Skills />
-            <ContactForm />
+          <Navbar />
+          <Mobilenav />
+          <Presentation />
+          <Experience />
+          <Skills />
+          <ContactForm />
         </BackgroundLines>
       </LanguageProvider>
     </ThemeProvider>
