1319e03325b59f65ea5ffb59585c9f3d9abfce1e
Manuel-latorre <manuel.latorre11@gmail.com>
2025-07-17 00:17:01 -0300

    Add ResumeButton component for linking to CV; styled for improved user interaction and accessibility.

---
+++ src/components/buttons/ResumeButton.tsx
@@ -0,0 +1,15 @@
+const ResumeButton = () => {
+
+  return (
+    <a 
+      href="/Manuel-Latorre-Frontend.pdf"
+      target="_blank"
+      rel="noopener noreferrer"
+      className="inline-flex items-center justify-center px-4 cursor-pointer bg-gradient-to-r from-[#C75305] via-[#FE9400] to-[#E34401] h-12 rounded-xl text-white text-base font-medium hover:translate-y-[-2px] transition-all duration-300 ease-out"
+    >
+        Echa un vistazo a mi CV
+    </a>
+  );
+};
+
+export default ResumeButton;
