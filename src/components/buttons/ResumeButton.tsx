02775faac628d325ac6abc1071fd0bc7eddbb6e4
Manuel-latorre <manuel.latorre11@gmail.com>
2025-07-26 11:38:28 -0300

    Update resume PDF and ResumeButton component for improved localization and content accuracy
    - Revised the resume PDF to include updated experiences, skills, and technologies, enhancing clarity and impact.
    - Modified the ResumeButton component to dynamically link to the correct resume PDF based on the selected language, ensuring a better user experience.

--- src/components/buttons/ResumeButton.tsx
+++ src/components/buttons/ResumeButton.tsx
@@ -4,9 +4,13 @@ import { translations } from "@/lib/translations";
 const ResumeButton = () => {
   const { language } = useLanguage();
 
+  const resumeHref = language === "en" 
+    ? "/ManuelLatorre-Frontend.pdf" 
+    : "/Manuel-Latorre-Frontend.pdf";
+
   return (
     <a 
-      href="/Manuel-Latorre-Frontend.pdf"
+      href={resumeHref}
       target="_blank"
       rel="noopener noreferrer"
       className="inline-flex items-center justify-center px-4 cursor-pointer bg-gradient-to-r from-[#C75305] via-[#FE9400] to-[#E34401] h-12 rounded-xl text-white text-base font-medium hover:translate-y-[-2px] transition-all duration-300 ease-out"
